527ba8f6ca652b9620d28d208aa8f908babca3dd
Chris Green <chrisgreen1993@gmail.com>
2024-07-07 22:01:28 +0100

    Update lottie player to stop animation resetting.
    This stops the animation from restarting when the animation object is updated.

--- components/LottiePlayer.tsx
+++ components/LottiePlayer.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import {
   Player,
   Controls,
   IPlayerProps,
+  PlayerEvent,
 } from "@lottiefiles/react-lottie-player";
+import { AnimationItem } from "lottie-web";
 
 interface LottiePlayerProps {
   src: IPlayerProps["src"];
 }
 
 export const LottiePlayer = ({ src }: LottiePlayerProps) => {
+  const playerRef = useRef<Player>(null);
+  const [lottie, setLottie] = useState<AnimationItem>();
+
+  const handleEvent = (event: PlayerEvent) => {
+    // When the animation is updated we want to keep the animation
+    // at the current frame, rather than it resetting to the start.
+    if (lottie && event === PlayerEvent.InstanceSaved) {
+      playerRef.current?.setSeeker(lottie?.currentFrame);
+    }
+  };
+
   return (
-    <Player src={src} loop className="h-[80vh]">
-      <Controls visible />
+    <Player
+      ref={playerRef}
+      lottieRef={setLottie}
+      onEvent={handleEvent}
+      src={src}
+      loop
+      className="h-[80vh]"
+    >
+      <Controls visible buttons={["play", "stop", "repeat", "frame"]} />
     </Player>
   );
 };
